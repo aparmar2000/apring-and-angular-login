7ddc0755f5a5a8d8d29978c6e331feb28dd0858e
aparmar2000 <aparmar2000@gmail.com>
2019-07-18 09:24:40 -0700

    Initial commit.

---
+++ src/test/java/com/asparmar/spring_angular_login/SpringAndAngularLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.asparmar.spring_angular_login;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAndAngularLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
