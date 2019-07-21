cb7acf6fe42a221c88c8e4bb35e4f4f9658105f3
aparmar2000 <aparmar2000@gmail.com>
2019-07-20 18:16:30 -0700

    Updated .gitignore

--- angularclient/src/app/service/app.service.ts
+++ angularclient/src/app/service/app.service.ts
@@ -21,6 +21,29 @@ export class AppService {
             } else {
                 this.authenticated = false;
             }
+			
+			console.log(response);
+			
+            return callback && callback();
+        });
+
+    }
+	
+  adduser(credentials, callback) {
+
+        const headers = new HttpHeaders(credentials ? {
+            authorization : 'Basic ' + btoa(credentials.username + ':' + credentials.password)
+        } : {});
+
+        this.http.get('http://localhost:8080/user', {headers: headers}).subscribe(response => {
+            if (response['name']) {
+                this.authenticated = true;
+            } else {
+                this.authenticated = false;
+            }
+			
+			console.log(response);
+			
             return callback && callback();
         });
 
